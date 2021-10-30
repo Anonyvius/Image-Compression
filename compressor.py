ca00a0ca94ec862e76db505e72052c6b7f1b9058
Anonyvius <30316592+Anonyvius@users.noreply.github.com>
2021-10-30 13:47:56 +0200

    optimization

--- compressor.py
+++ compressor.py
@@ -7,4 +7,4 @@ pixel = InputImage.load()
 width, height = InputImage.size
 for h in range(height):
     for w in range(width):
-        data.write(str(pixel[h, w]).replace(',', '').replace('(', '').replace(')', '') + '\n')
+        data.write(str(pixel[h, w]) + '\n')
--- decompressor.py
+++ decompressor.py
@@ -1,5 +1,4 @@
 from PIL import Image
-import math
 
 raw = open('picture', 'r').read().splitlines()
 counter = 0
@@ -9,8 +8,7 @@ newPixel = newPic.load()
 
 for h in range(256):
     for w in range(256):
-        pixel = raw[counter].split(' ')
-        newPixel[h, w] = (int(pixel[0]), int(pixel[1]), int(pixel[2]), int(pixel[3]))
+        newPixel[h, w] = eval(raw[counter])
         counter += 1
 
 newPic.save('newPic.png')
