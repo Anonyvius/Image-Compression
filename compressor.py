7fb180c944099140c8aca86f7d17aa2ba68ecb82
Anonyvius <30316592+Anonyvius@users.noreply.github.com>
2021-10-30 13:35:55 +0200

    basic functionality

--- compressor.py
+++ compressor.py
@@ -1,16 +1,10 @@
 from PIL import Image
 
-data = open('picture.txt', 'w+')
+data = open('picture', 'w+')
 
-InputImage = Image.open('input.png')  # Can be many different formats.
+InputImage = Image.open('input.png')
 pixel = InputImage.load()
 width, height = InputImage.size
-#print(width, height)
 for h in range(height):
     for w in range(width):
-        print(pixel[h, w])
-        #print(type(pixel[h, w]))
         data.write(str(pixel[h, w]).replace(',', '').replace('(', '').replace(')', '') + '\n')
-        pixel[h, w] = (100, 1, 90, 255)
-
-InputImage.save('alive_parrot.png')
--- decompressor.py
+++ decompressor.py
@@ -1,16 +1,16 @@
 from PIL import Image
+import math
 
-data = open('picture.txt', 'r').read().splitlines()
+raw = open('picture', 'r').read().splitlines()
+counter = 0
 
-newPic = Image.new('RGBA', (256, 256), color='white')
-#newPic.save('newPic.png')
+newPic = Image.new('RGBA', (256, 256))
 newPixel = newPic.load()
-for d in data:
-    pixel = d.split(' ')
-    newTuple = (pixel[0], pixel[1], pixel[2], pixel[3])
-    #print(newTuple)
-    for h in range(256):
-        for w in range(256):
-            newPixel[w, h] = 1
+
+for h in range(256):
+    for w in range(256):
+        pixel = raw[counter].split(' ')
+        newPixel[h, w] = (int(pixel[0]), int(pixel[1]), int(pixel[2]), int(pixel[3]))
+        counter += 1
 
 newPic.save('newPic.png')
